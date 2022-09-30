2aedc964fb673422fe923a173f4c6b6bdece6e9c
CarolineVicente <vicente.caroline@hotmail.com>
2022-09-30 16:44:22 -0300

    cadastro de produtos update

--- app/src/main/java/com/example/uana/FormFragment.kt
+++ app/src/main/java/com/example/uana/FormFragment.kt
@@ -6,9 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.uana.adapter.ProdutoAdapter
 import com.example.uana.databinding.FragmentFormBinding
-import com.example.uana.model.Produto
+
 
 class FormFragment : Fragment() {
 
